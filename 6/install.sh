1e1e9b434b6e89f839e727ec010c0a8d012cfe02
teCemka <kolesnikov.nn@yandex.ru>
2022-01-06 13:30:27 +0300

    added .sh and emptyfile

---
+++ 6/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ apt-get update
+ apt-get -y install python3
+ apt-get -y install python-pip
+ apt-get -y install python3-pip
+ pip install psycopg2-binary
+ pip install Django
